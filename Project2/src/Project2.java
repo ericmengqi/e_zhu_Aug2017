3e25cf9574d1249614ec70d9342c2bfed16c56bb
Eric Zhu <ericmengqi@gmail.com>
2017-08-22 09:28:09 -0700

    Second Project

---
+++ Project2/src/Project2.java
@@ -0,0 +1,15 @@
+
+// Eric Zhu
+// My second Java project. It will compare print and println.
+// August 22, 2017
+
+public class Project2 {
+
+	public static void main(String[] args) {
+		System.out.println ("hello -- this is project 2");
+		
+		System.out.print ("hello -- this is project 2");
+
+	}
+
+}
